59d3e503dbce41547c237fa95bcedd5f2edbd5c9
Zin KO <zinko.burmesegeek@outlook.com>
2020-10-09 19:12:13 +0630

    Python Auto bot

---
+++ Unlimited_Message_In_Messenger_With_Python.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+message = 5
+while message > 0:
+    time.sleep(1)
+    pyautogui.typewrite("Hello!")
+    time.sleep(1)
+    pyautogui.press('enter')
+    message = message - 1
+
