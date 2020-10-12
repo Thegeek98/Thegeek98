0f8eccb323247cfded7b81c1b97204d407f90088
Zin KO <zinko.burmesegeek@outlook.com>
2020-10-13 00:29:29 +0630

    Brute_force_crack tools 13.10.2020

---
+++ RAR_password_crack.py
@@ -0,0 +1,25 @@
+import rarfile
+from rarfile import RarWrongPassword
+import random
+
+rarfile.UNRAR_TOOL = "C:\\Program Files\\WinRAR\\UnRaR.exe"
+
+rar_file = input("Enter your rar file name :")
+
+r = rarfile.RarFile(f'{rar_file}.rar')
+
+guess_password = ""
+
+while (True):
+
+    guess_password = str(random.randint(0, 999))
+
+    print("*****", guess_password, "******")
+
+    try:
+        r.extractall(pwd=guess_password)
+        print("RaR Password is : ", guess_password)
+        break
+    except RarWrongPassword as err:
+        print("error", err)
+        continue
