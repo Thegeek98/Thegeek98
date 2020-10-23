13021f3082b56cdd5ddad82cb38db886ebe024f3
Zin KO <zinko.burmesegeek@outlook.com>
2020-10-23 12:00:07 +0630

    Test_function 23.10.2020

---
+++ Wifi_hotspot_enable_function_test.py
@@ -0,0 +1,41 @@
+# Wi-Fi Hostspot enabler for window
+# Programmer :  Zin KO
+# Date :        7.10.2020
+
+import os
+
+def enable_wifi_hotspot():
+    print("Starting Wi-Fi Hotspot ..... ")
+    os.system("netsh wlan set hostednetwork mode=allow ssid=" + ssid + " key=" + passwd)
+    os.system("netsh wlan start hostednetwork")
+
+def disable_wifi_hotspot():
+    print("Stoping Wi-Fi Hotspot ..... ")
+    os.system("netsh wlan stop hostednetwork")
+
+def main():
+    os.system('cls')
+    print('\n\n\n')
+    print('Burmese Geek Wifi Hostspot')
+    print('(c) 2020 Burmese Geek Group. All right reserved.')
+    print()
+
+    ssid = input("Please Enter Wi-Fi Name: ")
+    passwd = int(input("Please Give the Password: "))
+    cmd = '0'
+    while cmd !='3':
+        print('1. Start Hotspot')
+        print('2. Stop Hotspot')
+        print('3. Exit')
+        cmd = input('Please Enter Your Choice (1,2,3): ')
+        if cmd == '1':
+            enable_wifi_hotspot()
+        elif cmd == '2':
+            disable_wifi_hotspot()
+        elif cmd == '3':
+            print("Exiting program .... ")
+            exit()
+        else:
+            print("Bad input! Please try again (only 1,2,3)")
+            os.system('pause')
+
