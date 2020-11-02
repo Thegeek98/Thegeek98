fd317645ea6d3c0f945bf69e462d2cfce7dc763b
Zin KO <zinko.burmesegeek@outlook.com>
2020-11-02 22:55:16 +0630

    File I/O basic 2.11.2020

---
+++ File_IO.py
@@ -0,0 +1,9 @@
+#Open a File
+
+f = open ("Area_Of_Circle.py","r")
+print("Name of the file:", f.name)
+print("Closed or not:", f.closed)
+print("Opening mode:", f.mode)
+f.close()
+print("Closed or not:", f.closed)
+#print("Softspace flag:", fo.softspace)
